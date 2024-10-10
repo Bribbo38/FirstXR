779a1398eb30763ced8ce8ec90a519a873f20457
Bribbo38 <luca0gastaldi@gmail.com>
2024-10-10 10:07:12 +0200

    Added BestScores and Finish
    Added the save and display for 3 Best Scores, plus the finish after exiting the 'house'

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -7,14 +8,23 @@ public class ScoreManager : MonoBehaviour
     private TextMeshProUGUI currentScoreText;
     [SerializeField]
     private TextMeshProUGUI bestScoreText;
+    [SerializeField]
+    private TextMeshProUGUI bestScoreText2;
+    [SerializeField]
+    private TextMeshProUGUI bestScoreText3;
 
     private int currentScore;
     private int bestScore;
+    private int bestScore2;
+    private int bestScore3;
 
     void Start()
     {
         currentScore = 0;
         bestScore = PlayerPrefs.GetInt("bestScore", 0);
+        bestScore2 = PlayerPrefs.GetInt("bestScore2", 0);
+        bestScore3 = PlayerPrefs.GetInt("bestScore3", 0);
+
         UpdateUI();
     }
 
@@ -26,15 +36,38 @@ public class ScoreManager : MonoBehaviour
         UpdateUI();
     }
 
+    public void Finish()
+    {
+        if (currentScore > bestScore)
+        {
+            PlayerPrefs.SetInt("bestScore", currentScore);
+            PlayerPrefs.SetInt("bestScore2", bestScore);
+            PlayerPrefs.SetInt("bestScore3", bestScore2);
+        }
+        else if (currentScore > bestScore2)
+        {
+            PlayerPrefs.SetInt("bestScore2", currentScore);
+            PlayerPrefs.SetInt("bestScore3", bestScore2);
+        }
+        else if(currentScore > bestScore3)
+        {
+            PlayerPrefs.SetInt("bestScore3", currentScore);
+        }
+
+        SceneManager.LoadScene("BasicScene");
+    }
+
     private void UpdateUI()
     {
         currentScoreText.SetText("Score: " + currentScore);
         bestScoreText.SetText("Best: " + bestScore);
+        bestScoreText2.SetText("Best 2: " + bestScore2);
+        bestScoreText3.SetText("Best 3: " + bestScore3);
     }
 
-    public void Finish()
+    private void OnTriggerEnter(Collider other)
     {
-        if (currentScore > bestScore)
-            PlayerPrefs.SetInt("bestScore", currentScore);
+        if (other.gameObject.CompareTag("Player"))
+            Finish();
     }
 }
